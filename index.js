fea0d71ddd9a0db732739da6ce6d381da07b4bf2
Tanya Mishra <tm991920@gmail.com>
2025-03-04 16:19:58 +0530

    [styling started]

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const btn = document.getElementById("genPass")
 const newPass = document.getElementById("newPass")
 const slider = document.getElementById("range")
 const rangeVal = document.getElementById("rangeValue")
+const upperCase = document.getElementById("capLet")
+const lowerCase = document.getElementById("samLet")
+const number = document.getElementById("number")
+const symbol = document.getElementById("symbol")
+const copyBtn = document.getElementById("copyBtn")
+
+
 rangeVal.innerText = slider.value
 
 slider.addEventListener('input',(e)=>{
@@ -15,17 +22,42 @@ btn.addEventListener('click',()=>{
     let smallLetter = 'abcdefghijklmnopqrstuvwxyz'
     let numberStr = '0123456789'
     let symbolsStr = '~`!@#$%^&*()_+{}[]'
-    let finalStr = capitalLetter + smallLetter + numberStr + symbolsStr;
+    let finalStr = '';
+
+
+    if(upperCase.checked){
+        finalStr += capitalLetter;
+    }
+    if(lowerCase.checked){
+        finalStr += smallLetter;
+    }
+    if(number.checked){
+        finalStr += numberStr;
+    }
+    if(symbol.checked){
+        finalStr += symbolsStr;
+    }
+    if(finalStr === ''){
+        alert("Please Select atleast one field")
+    }
     let latestPass = ''
 
     
 
     for(let i=0; i<slider.value; i++){
-        latestPass += finalStr[i]
+        let randNum = Math.floor(Math.random() * finalStr.length)
+        //latestPass += finalStr[randNum]
+        latestPass += finalStr.charAt(randNum)
     }
 
 
 
 
    newPass.innerText = `${latestPass}`
+
+
+})
+
+copyBtn.addEventListener('click',()=>{
+    window.navigator.clipboard.writeText(newPass.innerText)
 })
